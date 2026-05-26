e20a6bd88801c97cc404bd973596c323b3c912e0
Javier Espinoza R. <Javier.espinoza1989@gmail.com>
2026-05-26 13:32:34 -0400

    se agrega footer con ifnormacion comercial , ventas y rrhh

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from "@/shared/navbar/navbar.component";
 import { SignInComponent } from "@/shared/components/sign-in/sign-in.component";
 import { SignUpModalComponent } from "@/shared/components/sign-up/sign-up.component";
 import { ForgotPasswordComponent } from "./shared/components/forgot-password/forgot-password.component";
+import { FooterComponent } from "./shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, SignInComponent, SignUpModalComponent, ForgotPasswordComponent],
+  imports: [RouterOutlet, NavbarComponent, SignInComponent, SignUpModalComponent, ForgotPasswordComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
